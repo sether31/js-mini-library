fb7bbf05809ad4f7b59c2a115a19597048e49bc7
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-01-24 11:04:15 +0800

    refactor(js): Refactor library implementation using ES6 classes
    Refactoring my constructor with prototype with ES6 classes.

--- script.js
+++ script.js
@@ -1,44 +1,86 @@
-const myLibrary = [
-  /*  {
-        title: 'To Kill a Mockingbird',
-        author: 'Harper Lee',
-        pages: 281,
-        read: 'Not Read'
-    },
-    {
-        title: 'The Great Gatsby',
-        author: 'F. Scott Fitzgerald',
-        pages: 180,
-        read: 'Read'
+class Book{
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
-        */
-];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+    toggleRead() {
+        this.read = !this.read;
+    }
 }
 
+
+class Library{
+    constructor(){
+        this.books = [];
+    }
+
+    addBook(book){
+        this.books.push(book);
+    }
+
+    deleteBook(index){
+        this.books.splice(index, 1);
+        this.renderBook();
+    }
+
+    renderBook(){
+        let tableBody = document.querySelector('.tableBody');
+        tableBody.innerHTML = '';
+
+        this.books.forEach((book, index)=>{
+            let newRow = document.createElement('tr');
+     
+             newRow.innerHTML = 
+             `
+                 <td style="text-align:left;">
+                     ${book.title}
+                 </td>
+                 <td style="text-align:left;">
+                     ${book.author}
+                 </td>
+                 <td>${book.pages}</td>
+                 <td onclick="library.toggleRead(${index})">
+                     <button type="button">
+                         ${book.read ? "Read" : "Not Read"}
+                     </button>
+                 </td>
+                 <td>
+                     <button type="button" onclick="library.deleteBook(${index})">Delete</button>
+                 </td>
+             `;
+             tableBody.appendChild(newRow);
+        });
+    }
+
+    toggleRead(index){
+        this.books[index].toggleRead();
+        this.renderBook();
+    }
+}
+
+// *library class
+const library = new Library();
 
 function addBookToLibrary() {
-  let title = document.querySelector('#bookTitle');
-  let author = document.querySelector('#author');
-  let pages = document.querySelector('#bookPages');
-  let read = document.querySelector('#read');
-
-  const newBook = new Book(title.value, author.value, pages.value, read.value);
-
-  myLibrary.push(newBook);
-  renderBook();
-
-  title.value = '';
-  author.value = '';
-  pages.value = '';
-  read.value = '';
+    const title = document.querySelector('#bookTitle').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#bookPages').value;
+    const read = document.querySelector('#read').value === true;
+    
+    const newBook = new Book(title, author, pages, read);
+    
+    library.addBook(newBook);
+    library.renderBook();
+    
+    // *clear fields
+    document.querySelector('#bookTitle').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#bookPages').value = '';
+    document.querySelector('#read').value = false;
 }
-
 
 const form = document.querySelector('.formBook');
 
@@ -49,54 +91,7 @@ document.querySelector('.addBook').addEventListener('click', ()=>{
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     addBookToLibrary();
+    form.style.display = 'none';
 });
 
-function renderBook(){
-   let tableBody = document.querySelector('.tableBody');
-
-   tableBody.innerHTML = '';
-
-   for(let i = 0; i < myLibrary.length; i++){
-    let newRow = document.createElement('tr');
-    let book = myLibrary[i];
-
-    newRow.innerHTML = 
-        `
-            <td style="text-align:left;">
-                ${book.title}
-            </td>
-            <td style="text-align:left;">
-                ${book.author}
-            </td>
-            <td>${book.pages}</td>
-            <td onclick="toggleRead(${i})">
-                <button type="button">
-                    ${book.read ? "Read" : "Not Read"}
-                </button>
-            </td>
-            <td>
-                <button type="button" onclick="deleteBook(${i})">Delete</button>
-            </td>
-        `;
-
-        tableBody.appendChild(newRow);
-   };
-}
-
-renderBook();
-
-function deleteBook(index){
-    myLibrary.splice(index, 1);
-    renderBook();
-}
-
-
-Book.prototype.isRead = function(){
-    this.read = !this.read;
-}
-
-function toggleRead(i){
-    myLibrary[i].isRead();
-    renderBook();
-}
-
+library.renderBook();
